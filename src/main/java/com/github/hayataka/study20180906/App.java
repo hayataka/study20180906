c689d914e78d671e3c1a505b4ad7d874104ee63a
hayakawa <hayakawatakahiko@gmail.com>
2018-09-06 20:09:16 +0900

    test

--- src/main/java/com/github/hayataka/study20180906/App.java
+++ src/main/java/com/github/hayataka/study20180906/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String a = "aaa";
+    	
         System.out.println( "Hello World!" );
     }
+
+
+    String hello() {
+    	return "b";
+    }
+
+
 }
