d2fb882da821ba6a5de257f5707588895665037c
hayakawa <hayakawatakahiko@gmail.com>
2018-09-06 19:53:18 +0900

    初回コミット

---
+++ src/main/java/com/github/hayataka/study20180906/App.java
@@ -0,0 +1,13 @@
+package com.github.hayataka.study20180906;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
